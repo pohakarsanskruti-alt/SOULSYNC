8eff02e985e09ebde579382895dd2aaa13a296cc
Sanskruti Pohakar <pohakarsanskruti@gmail.com>
2026-07-21 13:39:56 +0530

    UPDATED FEATURES

--- bot1.py
+++ bot1.py
@@ -13,8 +13,6 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-if not BOT_TOKEN:
-    raise ValueError("BOT_TOKEN is missing from Railway VAriables")
 print("BOT TOKEN FOUND: ", BOT_TOKEN is not None)
 favorites = {}
 playlists = {}
